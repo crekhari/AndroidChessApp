eee1e45f3697561df77b25de41f306a0a5f19c32
Jahnavi Bavuluri <jbavuluri@gmail.com>
2021-04-21 01:01:44 -0400

    first commit

---
+++ app/src/main/java/com/example/androidchess52/PlayGame.java
@@ -0,0 +1,21 @@
+package com.example.androidchess52;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PlayGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.play_game);
+    }
+
+    public void onClick(View view) {
+        return;
+    }
+
+}
